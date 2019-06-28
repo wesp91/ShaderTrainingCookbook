7dd2a01eb0ae5ff68b196b353aa35ed50f067e90
daniele.montesano <daniele.montesano@supership.jp>
2019-06-28 17:18:58 +0900

    [add] hologram, circle and texture blending

---
+++ Assets/Chapter3/Scripts/SetRadiusProperties.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetRadiusProperties : MonoBehaviour
+{
+    [SerializeField] private Material _radiusMaterial;
+    [SerializeField] private float _radius = 1;
+    [SerializeField] private float _radiusWidth = 1;
+    [SerializeField] private Color _color = Color.white;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(_radiusMaterial != null)
+        {
+            _radiusMaterial.SetVector("_Center", transform.position);
+            _radiusMaterial.SetFloat("_Radius", _radius);
+            _radiusMaterial.SetFloat("_RadiusWidth", _radiusWidth);
+            _radiusMaterial.SetColor("_RadiusColor", _color);
+        }
+    }
+}
